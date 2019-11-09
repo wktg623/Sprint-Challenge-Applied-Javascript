4c64ff5b27bbef794c7d68c5bf6cf8e7c97e5953
wktg623 <wktg623@gmail.com>
2019-11-08 22:39:05 -0600

    not successfulgit add .!

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,27 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    const divHeader = document.createElement('div');
+    const spanDate = document.createElement('span');
+    const h1 = document.createElement('h1');
+    const spanTemp = document.createElement('span');
+
+    divHeader.classList.add('header');
+    spanDate.classList.add('date');
+    spanTemp.classList.add('temp');
+
+    divHeader.appendChild(spanDate);
+    divHeader.appendChild(h1);
+    divHeader.appendChild(spanTemp);
+    
+
+    spanDate.textContent = "March 28, 2019";
+    h1.textContent = "Lambda Times";
+    spanTemp.textContent = "98°";
+
+    return divHeader;
+}
+
+const headerContainer = document.querySelector('.header-container');
+headerContainer.appendChild(Header());
